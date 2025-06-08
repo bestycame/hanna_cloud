e9b1b58d85c696352d9f254fee3c4a4ad42ff0ab
Olivier d'Otreppe <odotreppe@abbove.com>
2025-06-08 21:14:18 +0200

    0.0.3

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name="hanna-cloud",
-    version="0.0.2",
+    version="0.0.3",
     author="Bestycame",
     author_email="bestycame@gmail.com",
     description="A Python client for the HannaCloud API",
