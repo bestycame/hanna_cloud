dc293878a8f599824d0934c376860e6bb53b0044
Olivier d'Otreppe <odotreppe@abbove.com>
2025-07-25 12:24:55 +0200

    Export errors

--- hanna_cloud/__init__.py
+++ hanna_cloud/__init__.py
@@ -3,8 +3,8 @@ HannaCloud API Client
 A Python client for interacting with the HannaCloud API.
 """
 
-__version__ = "0.0.4"
+__version__ = "0.0.5"
 
-from .client import HannaCloudClient
+from .client import HannaCloudClient, AuthenticationError, DeviceNotFoundError, APIError, RemoteHoldError, DeviceLogError, ValidationError
 
-__all__ = ["HannaCloudClient"]
+__all__ = ["HannaCloudClient", "AuthenticationError", "DeviceNotFoundError", "APIError", "RemoteHoldError", "DeviceLogError", "ValidationError"]
--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name="hanna-cloud",
-    version="0.0.4",
+    version="0.0.5",
     author="Bestycame",
     author_email="bestycame@gmail.com",
     description="A Python client for the HannaCloud API",
